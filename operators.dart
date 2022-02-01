4227c9216ac48289175c0d4ed0c99baddc904e5c
Vivek Rai <96563624+vivekrai1999@users.noreply.github.com>
2022-02-01 10:16:49 +0530

    Create operators.dart

---
+++ operators.dart
@@ -0,0 +1,56 @@
+// Arithmetic operator
+/*
+
++ add 
+- subtract
+% moduls or remainder
+* multiply
+
+*/
+
+// relational operator
+/*
+
+== equal to
+!= not equal to
+<=  less than equal to
+>=  greater than equal to
+
+*/
+
+// unary operator
+/*
+
+++num; pre increment
+num++; post increment
+num+=1;
+num-=1;
+
+*/
+
+// logical operator
+/*
+
+&& logical and
+|| logical or
+
+*/
+
+// Null aware operator
+/*
+
+?. used to check if an object is null
+??  used to assign a default value if the object is null
+??= used to assign a premanent default value if the varibale is null
+
+*/
+
+// tertary operator
+/*
+
+condition ? if condition is true : if condition is false;
+
+*/
+
+
+
